8fb875244a235abaa76800662b4f6c622e232744
Gerardo Lanza <glanza007@gmail.com>
2023-01-27 13:46:51 -0600

    Create Project

---
+++ Gerantlp/Gerantlp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gerantlp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
